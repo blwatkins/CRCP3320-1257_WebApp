0c26632ccdc00e13a15ae18a8fa9b0c72a40394a
blwatkins <blwatkins@users.noreply.github.com>
2025-10-16 18:17:58 -0500

    Add lecture example updates from October 16.

--- src/app.mjs
+++ src/app.mjs
@@ -1,16 +1,35 @@
 import express from 'express';
 
+import { encode } from 'html-entities';
+
 import { RandomColor } from './utils/random-color.mjs';
 import { RandomNumber } from './utils/random-number.mjs';
 
 const app = express();
 const port = 3000;
 
+const colors = [];
+
+app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/randomColor', (request, response) => {
   const hexColor = RandomColor.getRandomHex();
   response.send(`<html><head><title>Random Color</title></head><body style="background: ${hexColor};"></body></html>`);
+  console.log(encodeURI("<script>alert('XSS')</script>"));
+});
+
+app.get('/color/:colorName', (request, response) => {
+  let colorName = request.params.colorName;
+  colorName = encode(colorName);
+
+  const results = colors.filter((color) => color.name.toLowerCase() === colorName.toLowerCase());
+
+  if (results.length > 0) {
+    colorName = results[0].hex;
+  }
+
+  response.send(`<html><head><title>${colorName}</title></head><body style="background: ${colorName};"><h1>${colorName}</h1></body></html>`);
 });
 
 app.get('/api/randomColor', (request, response) => {
@@ -23,6 +42,25 @@ app.get('/api/randomNumber', (request, response) => {
   const maxValue = parseFloat(request.query.max);
   const randomNumber = RandomNumber.getRandomFloat(minValue, maxValue);
   response.json({ number: randomNumber });
+});
+
+app.post('/api/color', (request, response) => {
+  if (request.body && request.body.name && request.body.hex) {
+    const colorName = encode(request.body.name);
+    const colorHex = encode(request.body.hex);
+    // TODO - validate that the hex value is properly formatted
+    colors.push({ name: colorName, hex: colorHex });
+    response.status(200).send('Success');
+  } else {
+    // bad request
+    response.status(400).send('Bad Request');
+  }
+});
+
+// change - add next parameter
+app.use((error, request, response, next) => {
+  console.error(error);
+  response.status(500).send('Internal Server Error');
 });
 
 app.listen(port, () => {
--- src/utils/random-number.mjs
+++ src/utils/random-number.mjs
@@ -1,14 +1,21 @@
 export class RandomNumber {
     static getRandomFloat(value1, value2) {
-        if (typeof value1 === 'number' && typeof value2 === 'number') {
+        if (RandomNumber.isValidNumber(value1) && RandomNumber.isValidNumber(value2)) {
             const min = Math.min(value1, value2);
             const max = Math.max(value1, value2);
             return (Math.random() * (max - min)) + min;
-        } else if (typeof value1 === 'number' && typeof value2 !== 'number') {
+        } else if (RandomNumber.isValidNumber(value1)) {
             const max = value1;
+            return Math.random() * max;
+        } else if (RandomNumber.isValidNumber(value2)) { 
+            const max = value2;
             return Math.random() * max;
         } else {
             return Math.random();
         }
+    }
+
+    static isValidNumber(value) {
+        return typeof value === 'number' && !Number.isNaN(value) && Number.isFinite(value);
     }
 }
